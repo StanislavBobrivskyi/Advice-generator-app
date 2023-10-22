56aa7663ad97110abae0c6e36fc7c86138894134
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-10-22 15:36:47 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,20 +47,44 @@
 import React from 'react';
 import { useAdviceAPI } from './API';
 import { Button } from './Button';
-
+import {
+  Wrapper,
+  Title,
+  AdviceWrapper,
+  Advice,
+  Divider,
+  Line,
+  Icon,
+} from './Components.styled';
 export function App() {
   const { advice, handleNextAdviceClick } = useAdviceAPI();
 
   return (
-    <div className="App">
-      <h1>Порада № {advice ? advice.id : '...'}</h1>
+    <Wrapper className="App">
+      <Title>ADVICE # {advice ? advice.id : '...'}</Title>
 
       {advice && (
-        <div>
-          <p>{advice.advice}</p>
-        </div>
+        <AdviceWrapper>
+          <Advice>{advice.advice}</Advice>
+        </AdviceWrapper>
       )}
+      <Divider>
+        <Line></Line>
+        <Icon>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="16"
+            viewBox="0 0 20 16"
+            fill="none"
+          >
+            <rect width="6" height="16" rx="3" fill="#CEE3E9" />
+            <rect x="14" width="6" height="16" rx="3" fill="#CEE3E9" />
+          </svg>
+        </Icon>
+        <Line></Line>
+      </Divider>
       <Button onClick={handleNextAdviceClick} />
-    </div>
+    </Wrapper>
   );
 }
---
+++ src/components/Components.styled.jsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  width: 540px;
+  height: 332px;
+  flex-shrink: 0;
+  background: var(--Dark-Grayish-Blue, #313a48);
+  box-shadow: 30px 50px 80px 0px rgba(0, 0, 0, 0.1);
+`;
+export const Title = styled.h1`
+  color: var(--Neon-Green, #53ffaa);
+  text-align: center;
+  font-feature-settings: 'clig' off, 'liga' off;
+
+  font-family: Manrope;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  letter-spacing: 4.086px;
+`;
+
+export const AdviceWrapper = styled.div`
+  width: 444px;
+`;
+
+export const Advice = styled.p`
+  color: var(--Light-Cyan, #cee3e9);
+  text-align: center;
+  font-feature-settings: 'clig' off, 'liga' off;
+
+  /* Quote */
+  font-family: Manrope;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  letter-spacing: -0.3px;
+`;
+
+export const Divider = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Line = styled.div`
+  width: 196px;
+  height: 1px;
+  flex-shrink: 0;
+  background: var(--Grayish-Blue, #4f5d74);
+  margin: 0 16px; /* Встановіть бажаний відступ між лініями */
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const SVG = styled.svg`
+  border-radius: 50%;
+`;
+export const ButtonAdvice = styled.button`
+  position: fixed;
+  background-color: transparent;
+  border: none;
+  transform: translate(0%, 250%);
+  transition: box-shadow 0.3s, transform 0.3s;
+  border-radius: 50%;
+
+  & :hover {
+    box-shadow: 0 0 10px #53ffaa; /* Додамо тінь при наведенні */
+  }
+  & :active {
+  transform: scale(1.01);
+`;
