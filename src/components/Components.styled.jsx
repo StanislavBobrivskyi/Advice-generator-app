0a6146de35afe26d6046664e586a4372c69d0275
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-10-23 09:22:11 +0200

    add start Btn

--- src/components/Components.styled.jsx
+++ src/components/Components.styled.jsx
@@ -21,6 +21,22 @@ export const Wrapper = styled.div`
       max-width: 300px;
     `}
 `;
+export const StartWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  flex-shrink: 0;
+  background: var(--Dark-Grayish-Blue, #313a48);
+  box-shadow: 30px 50px 80px 0px rgba(0, 0, 0, 0.1);
+  height: 104px;
+`;
+
 export const Title = styled.h1`
   color: var(--Neon-Green, #53ffaa);
   text-align: center;
@@ -74,6 +90,19 @@ export const Icon = styled.div`
 export const SVG = styled.svg`
   border-radius: 50%;
 `;
+
+export const StartBtn = styled.button`
+  width: 150px;
+  border: none;
+  border-radius: 50%;
+  background-color: transparent;
+  & :hover {
+    box-shadow: 0 0 10px #53ffaa; /* Додамо тінь при наведенні */
+  }
+  & :active {
+  transform: scale(1.01);
+`;
+
 export const ButtonAdvice = styled.button`
   position: absolute;
   background-color: transparent;
